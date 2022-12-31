17d339a842400fca46b3651491c77c63f7951116
ritiksahni <ritiksahni0203@gmail.com>
2023-01-01 02:00:06 +0530

    Add service account creds to env

--- firebaseAdmin.py
+++ firebaseAdmin.py
@@ -1,9 +1,20 @@
+import os
+import json
+from dotenv import load_dotenv
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+
+load_dotenv('./.env')
+
+
 # Use the application default credentials
-cred = firebase_admin.credentials.Certificate('.speechnotes-ServiceKey.json')
+serviceAccount = os.getenv('GOOGLE_SERVICE_ACCOUNT_KEY');
+serviceAccountCredentials = json.loads(serviceAccount)
+
+cred = firebase_admin.credentials.Certificate(serviceAccountCredentials)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
